6af9f8dbdbc8d96e464f40ac5bcc7e024a3f54a4
unknown <mrollberg@ymail.com>
2017-09-01 14:14:14 -0500

    Implemented promote and parseFromFilePath

--- Employee.js
+++ Employee.js
@@ -3,14 +3,29 @@
 const fs = require('fs')
 
 class Employee {
-  constructor (/* ??? */) {
-    // TODO
+  constructor (name, title, salary) {
+    this.name = name
+    this.title = title
+    this.salary = salary
   }
 
+  promote(title, salary) {
+    this.title = title
+    this.salary = salary
+
+  }
+  
   // TODO ???
 }
 
 // TODO ???
+Employee.parseFromFilePath = (path) => {
+  const jsonEmployee = JSON.parse(fs.readFileSync(path))
+  return new Employee(jsonEmployee.name, jsonEmployee.title, jsonEmployee.salary)
+
+}
+
+
 
 module.exports = {
   Employee
